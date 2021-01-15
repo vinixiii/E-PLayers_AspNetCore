0cc9205e3fdf7c952cbfddb2b79bb5e6e31f6b40
Vinícius <vinissfigueiroa@gmail.com>
2021-01-14 22:40:14 -0300

    Parte 4 - Upload de imagem / Parte 5 - Exclusão de imagem

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using EPlayers_AspNetCore.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -32,12 +33,53 @@ namespace E_PLayers_AspNetCore.Controllers
             Equipe novaEquipe = new Equipe();
             novaEquipe.IdEquipe = Int32.Parse(form["IdEquipe"]);
             novaEquipe.Nome = form["Nome"];
-            novaEquipe.Imagem = form["Imagem"];
+            //Início do Upload de Imagem
+
+            //Todo elemento do tipo IFormCollection tem a propriedade File que identifica o upload de arquivos. Essa propriedade contém índices assim como um array.
+            //Verificamos se o usuário selecionou um arquvio
+            if (form.Files.Count > 0)
+            {
+                //Recebemos o arquivo que o usuário enviou e armazenamos na variável file
+                var file = form.Files[0];
+                //Indicando o caminho em que a pasta será criada
+                var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
+
+                //Verificamos se o diretório (pasta) existe, se não existe, será criada!
+                if(!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+
+                //                      localhost:5001                                  Equipes   imagem.png        
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/", folder, file.FileName);
+
+                //Utilizaremos o using para abrir e fechar o documetno automaticamente
+                //FileStream permite a manipulação de arquivos
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                novaEquipe.Imagem = file.FileName;
+            } else {
+                novaEquipe.Imagem = "padrao.png";
+            }
+            //Final do Upload de Imagem
 
             //Cadastramos a equipe no CSV
             equipeModel.Create(novaEquipe);
             ViewBag.Equipes = equipeModel.ReadAll();
 
+            return LocalRedirect("~/Equipe/Listar");
+        }
+
+        [Route("{id}")]
+        public IActionResult Excluir(int id)
+        {
+            //Deletar a equipe
+            equipeModel.Delete(id);
+            //Atualizar a lista
+            ViewBag.Equipes = equipeModel.ReadAll();
             return LocalRedirect("~/Equipe/Listar");
         }
     }
